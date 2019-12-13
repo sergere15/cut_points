fdacea04a985fa2bc7e29701ca58e1067dacedf3
sergere15 <43388544+sergere15@users.noreply.github.com>
2019-12-13 20:38:31 +0300

    Added test on double input. Some fixes.
    Transform main part of algorithm into function. Added function that counts time on double input.

--- Source.cpp
+++ Source.cpp
@@ -106,7 +106,7 @@ void Graph_gen(bool** matrix, int n_, int edges) {
 	matrix[half][half - 1] = true;
 	matrix[half - 1][half] = true;
 
-	//if (n < 50) print_graph(matrix, n);
+	
 
 }
 
@@ -122,13 +122,10 @@ void matrix_to_list(vector<vector<int>>& list, bool** matrix, int n) {
 	for (int p = 0; p < n; p++) {
 		delete[] matrix[p];
 	}
-
-	//if (n < 50) print_graph(list, n);
 }
 
 void IS_CUTPOINT(int v) {
-	if (0)
-		cout << "Cut point: " << v << endl;
+		//cout << "Cut point: " << v << endl; //Выввод в поток сильно влияет на время
 }
 
 void dfs(int v, int p, int n, bool** g, int timer, bool* used, int* tin, int* fup) {
@@ -150,12 +147,12 @@ void dfs(int v, int p, int n, bool** g, int timer, bool* used, int* tin, int* fu
 		else {
 			dfs(to, p, n, g, timer, used, tin, fup);
 			fup[v] = min(fup[v], fup[to]);
-			if (fup[to] >= tin[v] && p != -1)
+			if (fup[to] >= tin[v] && p != -1)//Стандартное условие алгоритма
 				IS_CUTPOINT(v);
 			++children;
 		}
 	}
-	if (p == -1 && children > 1)
+	if (p == -1 && children > 1)//Проверка при условии, что вершина - корень
 		IS_CUTPOINT(v);
 }
 
@@ -183,31 +180,30 @@ void dfs_list(int v, int p, int n, vector<vector<int>>& g, int timer, bool* used
 
 }
 
-int main()
-{
-
+void main_part(int i_, int k_) {
 	int n = 0;
 	int edges = 0;
 
-	for (int i = 1; i <= 4; i++) {
+	// Проверка на указанных входных данных
+	for (int i = i_; i <= i_ + 3 ; i++) {
 		n = (i + 1) * 113; //Генерация кол-ва вершин
 
-		//Создание массивов, необходимых для работы алгоритма
+						   //Создание массивов, необходимых для работы алгоритма
 		bool* used = new bool[n];
 		memset(used, false, n);
 		int timer = 0;
 		int* tin = new int[n];
 		int* fup = new int[n];
 
-		for (int k = 13; k <= 17; k++) {
+		for (int k = k_; k <= k_ + 4; k++) {
 
 			edges = (n - 1) + floor(3.7 * k * k);//Генерация кол-ва ребер
 
-		  //Обнуление массивов, которые заполнялись на предыдущей итерации
-			memset(used, false, n);	
+												 //Обнуление массивов, которые заполнялись на предыдущей итерации
+			memset(used, false, n);
 			timer = 0;
-			
-			
+
+
 			cout << "/////////////////////////////////////////////" << endl;
 			cout << "Number of vertex: " << n << "  edges: " << edges << endl;
 
@@ -223,14 +219,14 @@ int main()
 			//Генерация входных данных
 			Graph_gen(matrix, n, edges);
 			matrix_to_list(list, matrix, n); //Convert matrix to list and delete matrix from memory
-
+			if (n < 15) print_graph(list, n);//вывод полученного графа
 											 //dfs(0, -1, n, matrix, timer, used, tin, fup);
 
 			unsigned long long sum = 0;
 
 			for (int t = 0; t < 10; t++) {
 				//if (t == 0) print_graph(list, n);
-				
+
 				memset(used, false, n);
 				timer = 0;
 
@@ -251,6 +247,86 @@ int main()
 		delete[] fup;
 
 	}
+}
+
+void main_part_double(int i_, int k_) {
+	int n = 0;
+	int edges = 0;
+
+	// Проверка на указанных входных данных
+	for (int i = i_; i <= i_ + 3; i++) {
+		n = (i + 1) * 113 * 2; //Генерация кол-ва вершин
+
+						   //Создание массивов, необходимых для работы алгоритма
+		bool* used = new bool[n];
+		memset(used, false, n);
+		int timer = 0;
+		int* tin = new int[n];
+		int* fup = new int[n];
+
+		for (int k = k_; k <= k_ + 4; k++) {
+
+			edges = (((n - 1) / 2) + floor(3.7 * k * k)) * 2;//Генерация кол-ва ребер
+
+												 //Обнуление массивов, которые заполнялись на предыдущей итерации
+			memset(used, false, n);
+			timer = 0;
+
+
+			cout << "/////////////////////////////////////////////" << endl;
+			cout << "Number of vertex: " << n << "  edges: " << edges << endl;
+
+			bool** matrix = new bool*[n];
+			for (int i1 = 0; i1 < n; i1++) {
+				matrix[i1] = new bool[n];
+				memset(matrix[i1], false, n);
+			}
+
+			vector<vector<int>> list;
+			list.resize(n);
+			list.shrink_to_fit();
+			//Генерация входных данных
+			Graph_gen(matrix, n, edges);
+			matrix_to_list(list, matrix, n); //Convert matrix to list and delete matrix from memory
+			if (n < 15) print_graph(list, n);//вывод полученного графа
+											 //dfs(0, -1, n, matrix, timer, used, tin, fup);
+
+			unsigned long long sum = 0;
+
+			for (int t = 0; t < 10; t++) {
+				//if (t == 0) print_graph(list, n);
+
+				memset(used, false, n);
+				timer = 0;
+
+				size_t start = clock();
+
+				//Вызов самого алгоритма
+				dfs_list(0, -1, n, list, timer, used, tin, fup);
+
+				size_t end = clock();
+				sum += (end - start);
+				print = false;
+			}
+			cout << "time of execution is " << (sum / 10000.) << " s" << endl << endl;
+			print = true;
+		}
+		delete[] used;
+		delete[] tin;
+		delete[] fup;
+
+	}
+}
+
+int main()
+{
+
+	cout << "Standart input: " << endl;
+	main_part(1, 13);// запускается на изначальных входных данных
+	cout << endl << endl << endl;
+	cout << "Input doubled: " << endl;
+	main_part_double(1, 13);// запускается на удвоенных данных
+	
 	system("pause");
 	return 0;
 }
